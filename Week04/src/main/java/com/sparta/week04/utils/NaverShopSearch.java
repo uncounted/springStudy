3ee94bbeee74cb7cbe544fb8a89f1957c76843c6
hoholoudly <hoholoudly@gmail.com>
2022-05-24 10:07:07 +0900

    4주차

---
+++ Week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -0,0 +1,44 @@
+package com.sparta.week04.utils;
+
+import com.sparta.week04.models.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component // 이제부터, @RequiredArgsConstructor 와 함께 사용할 경우 스프링이 자동으로 생성합니다.
+public class NaverShopSearch {
+    public String search(String query) {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "zdqMoIkFaK8uKvC2oNY2");
+        headers.add("X-Naver-Client-Secret", "LiZfsgtuD5");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public List<ItemDto> fromJSONtoItems(String result) {
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items  = rjson.getJSONArray("items");
+        List<ItemDto> ret = new ArrayList<>();
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = (JSONObject) items.get(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            ret.add(itemDto);
+        }
+        return ret;
+    }
+}
